8702e88e0938a12a08b9e8c1c3f9309b9931fd90
10rdan <10rdantejada@gmail.com>
2024-06-13 19:25:06 +0000

    implemented a few scientific functions

--- src/script.js
+++ src/script.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     console.log('Document is ready');
     const display = document.getElementById('calc-display');
     const buttons = document.getElementsByClassName('btn');
     console.log(display);
     console.log(buttons);
 
-    buttonArray = Array.from(buttons);   
-    
+    buttonArray = Array.from(buttons);
+
     let currentValue = "";
 
     function evaluateResult() {
         console.log('currentValue: ', currentValue)
-        const convertedValue = currentValue.replace("x", "*").replace("÷", "/").replace("%", "*0.01");
+        const convertedValue = currentValue.replace("x", "*").replace("÷", "/").replace("%", "*0.01").replace('sin', 'Math.sin').replace('cos', 'Math.cos').replace('ln','Math.log').replace('π', Math.PI).replace('log','Math.log10').replace('e','Math.E').replace('tan', 'Math.tan').replace('√', 'Math.sqrt');
         console.log('convertedValue: ', convertedValue);
         const result = eval(convertedValue);
         currentValue = result.toString();
@@ -20,24 +20,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
     buttonArray.forEach(function (button) {
         console.log('button clicked', button.innerText);
-        button.addEventListener('click', function() {
-        console.log('first button clicked');
-        const value = button.innerText;
+        button.addEventListener('click', function () {
+            console.log('first button clicked');
+            const value = button.innerText;
 
-        if (value == "AC") {
-            currentValue = "";
-            display.value = currentValue;
-        } else if(value === "=") {
-            evaluateResult();
-        }
-        
-        else {
-            currentValue += value;
-            console.log('currenvalue:', currentValue);
-            display.value = currentValue;
-        }
-    });
-    });
-
+            try {
+                if (value == "AC") {
+                    currentValue = "";
+                    display.value = currentValue;
+                } else if (value === "=") {
+                    evaluateResult();
+                }
     
+                else {
+                    currentValue += value;
+                    console.log('currenvalue:', currentValue);
+                    display.value = currentValue;
+                }
+            } catch(error) {
+                console.error(error);
+                currentValue = "ERROR";
+                display.value = currentValue;
+            }    
+        });
+    });
+
+
 })
